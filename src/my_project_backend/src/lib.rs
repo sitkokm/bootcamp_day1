35d42b6f3b923ae02473447bdbf70cdf296e46e8
Katarzyna Sitko <xxyzxx.xx.xx@gmail.com>
2024-12-03 21:23:03 +0000

    initial commit

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,15 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| *msg.borrow_mut()= new_msg)
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+    format!("Hi, {}!", name)
 }
